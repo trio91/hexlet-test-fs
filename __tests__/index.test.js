7e52a387f5168e86952caeade2281db376acbc0e
Dima <ciba91@gmail.com>
2024-09-09 12:23:25 +0300

    made

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,24 @@
 import reverse from '../src/index.js';
 
-test('reverse', () => {
-  // TODO: здесь будет чтение файла и запись его содержимого в константу
-  const text = 'Hello';
-  const result = 'olleH';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { expect, test } from '@jest/globals';
 
-  expect(reverse(text)).toEqual(result);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('reverse', () => {
+  expect(reverse('hello')).toEqual('olleh');
+  expect(reverse('')).toEqual('');
+});
+
+test('reverse with log text', () => {
+  const text = readFixtureFile('starting.txt').trim();
+  const expected = readFixtureFile('ending.txt').trim();
+
+  expect(reverse(text)).toEqual(expected);
 });
